d0a42eee40061419a1647f148366365793c4c422
Simone Basso <bassosimone@gmail.com>
2015-03-18 00:53:43 +0100

    Fix the use after free bug
    After this commit we have the following:
    
    $ valgrind ./src/callbacks
    ==15765== Memcheck, a memory error detector
    ==15765== Copyright (C) 2002-2013, and GNU GPL'd, by Julian Seward et al.
    ==15765== Using Valgrind-3.10.0 and LibVEX; rerun with -h for copyright info
    ==15765== Command: ./src/callbacks
    ==15765==
    Construct 0x5e3f040
    Returned
    Connect 0x5e3f040
    Destruct 0x5e3f040
    ==15765==
    ==15765== HEAP SUMMARY:
    ==15765==     in use at exit: 0 bytes in 0 blocks
    ==15765==   total heap usage: 2 allocs, 2 frees, 240 bytes allocated
    ==15765==
    ==15765== All heap blocks were freed -- no leaks are possible
    ==15765==
    ==15765== For counts of detected and suppressed errors, rerun with: -v
    ==15765== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)

--- include/transport.hpp
+++ include/transport.hpp
@@ -36,10 +36,22 @@ public:
   void on_flush(std::function<void()> fn) { flush_handler = fn; }
   void on_error(std::function<void(int)> fn) { error_handler = fn; }
 
-  void emit_connect() { connect_handler(); }
-  void emit_data(std::string d) { data_handler(d); }
-  void emit_flush() { flush_handler(); }
-  void emit_error(int e) { error_handler(e); }
+  void emit_connect() {
+    auto h = connect_handler;
+    h();
+  }
+  void emit_data(std::string d) {
+    auto h = data_handler;
+    h(d);
+  }
+  void emit_flush() {
+    auto h = flush_handler;
+    h();
+  }
+  void emit_error(int e) {
+    auto h = error_handler;
+    h(e);
+  }
 
   int get_filedesc() { return filedesc; }
 
