21e5b2caafde05168431aae5310c47928f0aa82c
Simone Basso <bassosimone@gmail.com>
2015-03-17 22:41:21 +0100

    Initial commit

---
+++ include/pointer.hpp
@@ -0,0 +1,40 @@
+/*-
+ * This file is part of CXX Experiments.
+ *
+ * CXX Experiments is free software. See AUTHORS and LICENSE for more
+ * information on the copying conditions.
+ */
+#ifndef POINTER_HPP
+#define POINTER_HPP
+
+///
+/// \file pointer.hpp
+/// \brief Smart pointer
+///
+
+#include <memory>
+
+namespace cxx_experiments {
+
+template <typename T> class Pointer : public std::shared_ptr<T> {
+
+  using std::shared_ptr<T>::shared_ptr;
+
+public:
+  T *operator->() {
+    if (std::shared_ptr<T>::get() == nullptr) {
+      throw std::runtime_error("Null pointer deference");
+    }
+    return std::shared_ptr<T>::operator->();
+  }
+
+  typename std::add_lvalue_reference<T>::type operator*() {
+    if (std::shared_ptr<T>::get() == nullptr) {
+      throw std::runtime_error("Null pointer deference");
+    }
+    return std::shared_ptr<T>::operator*();
+  }
+};
+
+}
+#endif
