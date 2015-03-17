bf767b7921676c6c4770c7694ce510c1b0d628e8
Simone Basso <bassosimone@gmail.com>
2015-03-17 22:45:30 +0100

    Add constraints

---
+++ include/constraints.hpp
@@ -0,0 +1,30 @@
+/*-
+ * This file is part of CXX Experiments.
+ *
+ * CXX Experiments is free software. See AUTHORS and LICENSE for more
+ * information on the copying conditions.
+ */
+#ifndef ANTANI_HPP
+#define ANTANI_HPP
+
+///
+/// \file constraints.hpp
+/// \brief Constraints for objects
+///
+
+namespace cxx_experiments {
+
+class NonCopyable {
+public:
+  NonCopyable(NonCopyable &) = delete;
+  NonCopyable &operator=(NonCopyable &) = delete;
+};
+
+class NonMovable {
+public:
+  NonMovable(NonMovable &&) = delete;
+  NonMovable &operator=(NonMovable &&) = delete;
+};
+
+}
+#endif
