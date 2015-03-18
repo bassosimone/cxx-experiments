74970953ee005434c3259614e6254fd420630e7f
Simone Basso <bassosimone@gmail.com>
2015-03-18 01:12:03 +0100

    Explain the purpose of src/callbacks.cpp

--- src/callbacks.cpp
+++ src/callbacks.cpp
@@ -36,11 +36,29 @@ static MyTransport *create_transport() {
   transport->on_connect([transport]() {
     transport->close();
   });
+
+  //
+  // Here we only return the raw pointer and the reference count of
+  // transport is decremented because we leave the scope.
+  //
+  // However, transport is still alive because it is saved in the
+  // closure of the on_connect() callback.
+  //
+  // We use the raw pointer as dirty trick to use the transport from
+  // the main function without changing its refcnt.
+  //
   return transport.get();
 }
 
 int main() {
   auto pointer = create_transport();
   std::cout << "Returned\n";
+
+  //
+  // Now we emit connect() which triggers the above callback which
+  // closes the transport which in turn means that all the callbacks
+  // are reset. That is, the closure of the callbacks is erased,
+  // hence the last reference to transport is gone.
+  //
   pointer->emit_connect();
 }
